811620e4b8f3e2d8eba95e8f25d7895ea18f4782
kamishubaizumo <kamishubaizumo@yahoo.co.jp>
2023-04-10 07:30:43 +0900

    javaの勉強

---
+++ src/javaPractice/JavaPractice.java
@@ -0,0 +1,92 @@
+package javaPractice;
+
+public class JavaPractice {
+    public static void main(String[] args) {
+
+    	
+    	
+    	final double pi = 3.14;
+    	int pie = 3;
+    	
+    	System.out.println("半径"+ pie +"cmの面積は" + pie * pie * pi);
+    	System.out.println("直径"+ pie +"では、"+ pie * pi);
+    	
+    	
+    	
+    	   
+    	        int a = 5;
+    	        System.out.println(4 * 3);
+    	        System.out.println(15 % 4);
+    	        System.out.println(a + 7);
+    	        a = a+4;
+    	        System.out.println(a);
+    	        
+    	        
+    	        int b = 10;
+    	        System.out.println((5 + 8) * 3);
+    	        b++;
+    	        System.out.println(b);
+    	        b--;
+    	        System.out.println(b);
+    	   
+    	
+    	        
+    	        int d = 5 * 3;
+    	        int e = 10 / 3;
+    	        double f = 10.0 / 3.0;
+    	        int g = d * e;
+    	        System.out.println(d);
+    	        System.out.println(e);
+    	        System.out.println(f);
+    	        System.out.println(g);
+    	        
+    	        
+    	        
+    	        int h = 5;
+    	        int i = 8;
+    	        h += 3;
+    	        System.out.println(h);
+    	        i -= 4;
+    	        System.out.println(i);
+    	        
+    	        
+    	        int j = 8;
+    	        int k = 5;
+    	        System.out.println("j =" + 8 + "," + "k =" + 5);
+    	        System.out.println("j + k =" + (j+k));
+//    	        ()をつけないと、85になってしまう。
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	
+    }
+}
